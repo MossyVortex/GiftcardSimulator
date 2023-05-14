b6a722e5251cd3454752f990f2fb5c19a24e3a4d
MOSA <mosa20111430@gmail.com>
2023-05-14 14:21:53 +0300

    test1 done

---
+++ src/Classes/Account.java
@@ -0,0 +1,21 @@
+package Classes;
+public class Account {
+
+    private String username;
+    private int balance;
+    private int cardsRedeemed;
+
+    public Account(String username){
+        this.username = username;
+        this.balance = 0;
+        this.cardsRedeemed = 0;
+    }
+
+    // public String redeemCard(String code){
+
+    //     if(code)
+
+    // }
+
+    
+}
---
+++ src/Classes/Cards.java
@@ -0,0 +1,54 @@
+package Classes;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class Cards {
+
+    private String code;
+    private boolean isRedeemed;
+    private int giftAmount;
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789!#$%&";
+
+    public Cards(int giftAmount){
+        this.code = generateCode();
+        this.isRedeemed = false;
+        this.giftAmount = giftAmount;
+    }
+
+    public String generateCode(){
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(12);
+        for (int i = 0; i < 12; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+    public String getCode(){
+        return this.code;
+    }
+
+    public int getGiftAmount(){
+        return this.giftAmount;
+    }
+
+    public boolean getIsRedeemed(){
+        return this.isRedeemed;
+    }
+
+    // setters should not be used, once a card is created it should be locked.
+
+    public void setGiftAmount(int giftAmount){
+        this.giftAmount = giftAmount;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public void setIsRedeemed(boolean isRedeemed){
+        this.isRedeemed = isRedeemed;
+    }
+    
+}
